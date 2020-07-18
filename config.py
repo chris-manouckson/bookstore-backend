3fbfd9c39771024b81e1e0c646eaecda44cad4c7
Christian Manukyan <chris.manouckson@gmail.com>
2020-07-19 00:08:23 +0400

    fix sqlalchemy configuration

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config:
   """Base config"""
 
   JWT_SECRET_KEY = environ.get('JWT_SECRET_KEY')
+  SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class DevelopmentConfig(Config):
   DEBUG = True
@@ -17,11 +18,9 @@ class DevelopmentConfig(Config):
   SERVER_NAME = '127.0.0.1:' + environ.get('PORT')
 
   SQLALCHEMY_DATABASE_URI = environ.get('DEVELOPMENT_DATABASE_URI')
-  SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class ProductionConfig(Config):
   DEBUG = False
   TESTING = False
 
   SQLALCHEMY_DATABASE_URI = environ.get('PRODUCTION_DATABASE_URI')
-  SQLALCHEMY_TRACK_MODIFICATIONS = False
